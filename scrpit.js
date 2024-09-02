868b0686431e07f20673619541783c55db697c5f
Matheus Gonçalves Caicó <96189442+Matheus-Dev-Souza@users.noreply.github.com>
2024-09-02 16:13:23 -0300

    Add files via upload

---
+++ scrpit.js
@@ -0,0 +1,7 @@
+document.getElementById('cadastroButton').addEventListener('click', function() {
+    window.location.href = 'https://docs.google.com/forms/d/e/1FAIpQLSeg4NnWPjmWbaMaomOdzK3CWxjjnFKPd4rhX_hOlBBEU8bkQw/viewform?embedded=true';
+});
+
+document.getElementById('cadastroButton2').addEventListener('click', function() {
+    window.location.href = 'https://docs.google.com/forms/d/e/1FAIpQLSeg4NnWPjmWbaMaomOdzK3CWxjjnFKPd4rhX_hOlBBEU8bkQw/viewform?embedded=true';
+});
